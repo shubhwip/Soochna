f2306839d926c18e9dc3ef59799e99ad5b58646c
Ankit Jain <ankitjain.meone@gmail.com>
2017-12-02 11:34:48 +0530

    Initial project structure setup

---
+++ src/main/java/com/soochna/SoochnaApplication.java
@@ -0,0 +1,12 @@
+package com.soochna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoochnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoochnaApplication.class, args);
+	}
+}
