a9f2d42b8897a6d047bdc5c45f7c7b62203719a3
Ankit Jain <ankitjain.meone@gmail.com>
2017-12-02 17:18:36 +0530

    added Repository class for notice DB

---
+++ src/main/java/com/soochna/repositories/NoticeRepository.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.soochna.repositories;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.soochna.entity.Notice;
+
+/**
+ * @author ankit
+ *
+ */
+public interface NoticeRepository extends CrudRepository<Notice, Serializable> {
+
+	Notice findById(long id);
+
+	Notice findByNoticeTitle(String noticeTitle);
+
+	List<Notice> findByMinistryName(String ministryName);
+
+	List<Notice> findAllByOrderByCreatedTimeStampDesc();
+}
